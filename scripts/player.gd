658e87050d7befe7db534b05f9537e6bcaef4b10
= <gigi.donegatti@gmail.com>
2026-06-26 20:00:57 -0300

    Atualiza o movimento do player com maquina de estados e cria os sprites de botões

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,31 +1,105 @@
 extends CharacterBody2D
 
+@export var speed := 300.0
+const JUMP_VELOCITY := -400.0
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-var max_jumps = 2
+enum State {
+	IDLE,
+	WALK,
+	JUMP
+}
+
+var state = State.IDLE
+
 var jumps = 0
+var max_jumps = 2
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
+# Última direção pressionada
+var direction := 0
 
+
+func _physics_process(delta):
+
+	# Gravidade
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	else:
 		jumps = 0
-	
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and jumps < max_jumps:
+	# -------------------------
+	# Sistema de prioridade do último input
+	# -------------------------
+
+	if Input.is_action_just_pressed("left"):
+		direction = -1
+
+	if Input.is_action_just_pressed("right"):
+		direction = 1
+
+	if Input.is_action_just_released("left"):
+		if Input.is_action_pressed("right"):
+			direction = 1
+		elif !Input.is_action_pressed("left"):
+			direction = 0
+
+	if Input.is_action_just_released("right"):
+		if Input.is_action_pressed("left"):
+			direction = -1
+		elif !Input.is_action_pressed("right"):
+			direction = 0
+
+	# -------------------------
+	# Pulo
+	# -------------------------
+
+	if Input.is_action_just_pressed("jump") and jumps < max_jumps:
 		velocity.y = JUMP_VELOCITY
 		jumps += 1
+		set_state(State.JUMP)
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("left", "right")
-	if direction:
-		velocity.x = direction * SPEED
+	# -------------------------
+	# Estados
+	# -------------------------
+
+	if is_on_floor():
+		if direction == 0:
+			set_state(State.IDLE)
+		else:
+			set_state(State.WALK)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		set_state(State.JUMP)
+
+	# -------------------------
+	# Executa estado
+	# -------------------------
+
+	match state:
+		State.IDLE:
+			idle_state()
+
+		State.WALK:
+			walk_state()
+
+		State.JUMP:
+			jump_state()
 
 	move_and_slide()
+
+
+func set_state(new_state):
+	if state == new_state:
+		return
+
+	state = new_state
+
+
+func idle_state():
+	velocity.x = move_toward(velocity.x, 0, speed)
+
+
+func walk_state():
+	velocity.x = direction * speed
+
+
+func jump_state():
+	velocity.x = direction * speed
